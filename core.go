4518d5ac3d940b453cfd85d6002c9fbfec2fa46e
Gonçalo Mendes Cabrita <goncalo@mendescabrita.com>
2026-01-02 17:01:03 +0000

    Link all curl libs statically for linux/arm64 to enable cross-compilation

--- core.go
+++ core.go
@@ -9,7 +9,7 @@ package curl
 #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/libs/linux_amd64_gnu -Wl,-Bstatic -lcurl-impersonate -Wl,-Bdynamic -lssl -lcrypto -lngtcp2_crypto_boringssl -lngtcp2 -lnghttp3 -lnghttp2 -lzstd -lbrotlienc -lbrotlidec -lbrotlicommon -lz -lstdc++ -ldl -lpthread
 
 #cgo linux,arm64 CFLAGS: -I${SRCDIR}/libs/include -DCURL_STATICLIB
-#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/libs/linux_arm64_gnu -Wl,-Bstatic -lcurl-impersonate -Wl,-Bdynamic -lssl -lcrypto -lngtcp2_crypto_boringssl -lngtcp2 -lnghttp3 -lnghttp2 -lzstd -lbrotlienc -lbrotlidec -lbrotlicommon -lz -lcares -lstdc++ -ldl -lpthread
+#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/libs/linux_arm64_gnu -Wl,-Bstatic -lcurl-impersonate -lssl -lcrypto -lngtcp2_crypto_boringssl -lngtcp2 -lnghttp3 -lnghttp2 -lzstd -lbrotlienc -lbrotlidec -lbrotlicommon -lz -lcares -Wl,-Bdynamic -lstdc++ -ldl -lpthread
 
 #include <stdlib.h>
 
